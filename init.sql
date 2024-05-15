40c13538e4963c86d9627e02821ab69d113a91d6
Hubert Gruniaux <hubert@gruniaux.com>
2024-05-15 11:16:44 +0200

    Add init.sql

---
+++ init.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS Persons;
+DROP TABLE IF EXISTS Curriculums;
+DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Grades;
+
+CREATE TABLE Persons
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    address VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL
+);
+
+CREATE TABLE Curriculums
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    secretary SERIAL NOT NULL REFERENCES Persons(id),
+    director SERIAL NOT NULL REFERENCES Persons(id)
+);
+
+
+CREATE TABLE Courses
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    teacher SERIAL NOT NULL REFERENCES Persons(id)
+);
+
+CREATE TABLE Validations
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    coefficient FLOAT NOT NULL,
+    date DATE NOT NULL,
+    course SERIAL NOT NULL REFERENCES Courses(id)
+);
+
+CREATE TABLE Grades
+(
+    student SERIAL NOT NULL REFERENCES Persons(id),
+    validation SERIAL NOT NULL REFERENCES Validations(id),
+    grade FLOAT NOT NULL,
+    PRIMARY KEY (student, validation)
+);
